323b8be6cb662e829a9897cc7e82eabfcb6c34d2
astfalckl <astfalckl@gmail.com>
2023-06-13 15:06:39 +0800

    finished setting up

---
+++ R/whittle.R
@@ -0,0 +1,19 @@
+#' Whittle Likelihood
+#'
+#' @param psd Power spectral density
+#' @param periodogram Periodogram
+#' @param return_log Return the log likelihood
+#'
+#' @return The value of the Whittle likelihood
+#' @export
+#'
+#' @examples
+whittle <- function(psd, periodogram, return_log = TRUE) {
+    wll <- sum(log(psd) + (periodogram / 2) / psd)
+
+    if (return_log) {
+        wll
+    } else {
+        exp(wll)
+    }
+}
---
+++ helpR.R
@@ -0,0 +1,9 @@
+
+setwd("/Users/00077063/Documents/PostDoc/20220903_bandlimited_gps/dwelch")
+
+library(devtools)
+library(ggplot2)
+library(tidyverse)
+library(dwelch)
+
+ggplot()
