10bc9d494c9221fe743e5ffd3efa7ae69ad39cb7
astfalckl <astfalckl@gmail.com>
2023-06-26 16:25:00 +0800

    fixed bochner function

--- R/bochner.R
+++ R/bochner.R
@@ -9,16 +9,19 @@
 #' @export
 #'
 #' @examples
-bochner <- function(acf, delta = 1, bias = FALSE) {
+bochner <- function(acf, delta = 1, h = NULL) {
     n <- length(acf)
     nfreq <- get_nfreq(n)
 
-    if (bias) {
-        acf <- (1 - (0:nfreq) / n) * acf
+    if (is.null(h)) {
+        acf <- (1 - (0:(n - 1)) / n) * acf
+    } else {
+        h <- h / sqrt(sum(h^2)) # normalise h
+        stats::convolve(h, h, type = "open")[n:(2 * n - 1)]
     }
 
     acf <- c(acf[1] / 2, acf[2:(n - 1)], acf[n] / 2)
 
-    2 * delta * Re(stats::fft(acf))[1:nfreq + 1]
+    2 * delta * Re(stats::fft(acf))[2:(nfreq + 1)]
 
 }
